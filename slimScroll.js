fa68775f4e6f218246633fb15023fbf685ae0cd7
Robin Komiwes <odisseus@gmail.com>
2011-11-23 11:39:58 +0100

    Added spaces after each inline comment start.

--- slimScroll.js
+++ slimScroll.js
@@ -11,7 +11,7 @@
 		slimScroll: function(o) {
 
 			var ops = o;
-			//do it for every element that matches selector
+			// do it for every element that matches selector
 			this.each(function(){
 
 			var isOverPanel, isOverBar, isDragg, queueHide, barHeight,
@@ -33,10 +33,10 @@
 				railColor = o.railColor || '#333',
 				railOpacity = o.railOpacity || 0.2;
 			
-				//used in event handlers and for better minification
+				// used in event handlers and for better minification
 				var me = $(this);
 
-				//wrap content
+				// wrap content
 				var wrapper = $(divS).css({
 					position: 'relative',
 					overflow: 'hidden',
@@ -44,14 +44,14 @@
 					height: cheight
 				}).attr({ 'class': 'slimScrollDiv' });
 
-				//update style for the div
+				// update style for the div
 				me.css({
 					overflow: 'hidden',
 					width: cwidth,
 					height: cheight
 				});
 
-				//create scrollbar rail
+				// create scrollbar rail
 				var rail  = $(divS).css({
 					width: size,
 					height: '100%',
@@ -64,7 +64,7 @@
 					zIndex: 90
 				});
 
-				//create scrollbar
+				// create scrollbar
 				var bar = $(divS).attr({ 
 					'class': 'slimScrollBar ', 
 					style: 'border-radius: ' + size 
@@ -81,19 +81,19 @@
 						zIndex: 99
 				});
 
-				//set position
+				// set position
 				var posCss = (position == 'right') ? { right: distance } : { left: distance };
 				rail.css(posCss);
 				bar.css(posCss);
 
-				//wrap it
+				// wrap it
 				me.wrap(wrapper);
 
-				//append to parent div
+				// append to parent div
 				me.parent().append(bar);
 				me.parent().append(rail);
 
-				//make it draggable
+				// make it draggable
 				bar.draggable({ 
 					axis: 'y', 
 					containment: 'parent',
@@ -101,26 +101,26 @@
 					stop: function() { isDragg = false; hideBar(); },
 					drag: function(e) 
 					{ 
-						//scroll content
+						// scroll content
 						scrollContent(0, $(this).position().top, false);
 					}
 				});
 
-				//on rail over
+				// on rail over
 				rail.hover(function(){
 					showBar();
 				}, function(){
 					hideBar();
 				});
 
-				//on bar over
+				// on bar over
 				bar.hover(function(){
 					isOverBar = true;
 				}, function(){
 					isOverBar = false;
 				});
 
-				//show on parent mouseover
+				// show on parent mouseover
 				me.hover(function(){
 					isOverPanel = true;
 					showBar();
@@ -132,7 +132,7 @@
 
 				var _onWheel = function(e)
 				{
-					//use mouse wheel only when mouse is over
+					// use mouse wheel only when mouse is over
 					if (!isOverPanel) { return; }
 
 					var e = e || window.event;
@@ -141,10 +141,10 @@
 					if (e.wheelDelta) { delta = -e.wheelDelta/120; }
 					if (e.detail) { delta = e.detail / 3; }
 
-					//scroll content
+					// scroll content
 					scrollContent(delta, true);
 
-					//stop window scroll
+					// stop window scroll
 					if (e.preventDefault && !releaseScroll) { e.preventDefault(); }
 					if (!releaseScroll) { e.returnValue = false; }
 				}
@@ -155,19 +155,19 @@
 
 					if (isWheel)
 					{
-						//move bar with mouse wheel
+						// move bar with mouse wheel
 						delta = bar.position().top + y * wheelStep;
 
-						//move bar, make sure it doesn't go out
+						// move bar, make sure it doesn't go out
 						delta = Math.max(delta, 0);
 						var maxTop = me.outerHeight() - bar.outerHeight();
 						delta = Math.min(delta, maxTop);
 
-						//scroll the scrollbar
+						// scroll the scrollbar
 						bar.css({ top: delta + 'px' });
 					}
 
-					//calculate actual scroll amount
+					// calculate actual scroll amount
 					var percentScroll = parseInt(bar.position().top) / (me.outerHeight() - bar.outerHeight());
 					delta = percentScroll * (me[0].scrollHeight - me.outerHeight());
 
@@ -178,10 +178,10 @@
 						bar.css({ top: offsetTop + 'px' });
 					}
 
-					//scroll content
+					// scroll content
 					me.scrollTop(delta);
 
-					//ensure bar is visible
+					// ensure bar is visible
 					showBar();
 
           // trigger hide when scroll is stopped
@@ -201,28 +201,28 @@
 					}
 				}
 
-				//attach scroll events
+				// attach scroll events
 				attachWheel();
 
 				var getBarHeight = function()
 				{
-					//calculate scrollbar height and make sure it is not too small
+					// calculate scrollbar height and make sure it is not too small
 					barHeight = Math.max((me.outerHeight() / me[0].scrollHeight) * me.outerHeight(), minBarHeight);
 					bar.css({ height: barHeight + 'px' });
 				}
 
-				//set up initial height
+				// set up initial height
 				getBarHeight();
 
 				var showBar = function()
 				{
-					//recalculate bar height
+					// recalculate bar height
 					getBarHeight();
 					clearTimeout(queueHide);
 					
-					//show only when required
+					// show only when required
 					if(barHeight >= me.outerHeight()) {
-						//allow window scroll
+						// allow window scroll
 						releaseScroll = true;
 						return;
 					}
@@ -232,7 +232,7 @@
 
 				var hideBar = function()
 				{
-					//only hide when options allow it
+					// only hide when options allow it
 					if (!alwaysVisible)
 					{
 						queueHide = setTimeout(function(){
@@ -245,24 +245,24 @@
 					}
 				}
 
-				//check start position
+				// check start position
 				if (start == 'bottom') 
 				{
-					//scroll content to bottom
+					// scroll content to bottom
 					bar.css({ top: 'auto', bottom: 0 });
 					scrollContent(0, true);
 				}
 				else if (typeof start == 'object')
 				{
-					//scroll content
+					// scroll content
 					scrollContent($(start).position().top, null, true);
 
-					//make sure bar stays hidden
+					// make sure bar stays hidden
 					if (!alwaysVisible) { bar.hide(); }
 				}
 			});
 			
-			//maintain chainability
+			// maintain chainability
 			return this;
 		}
 	});
