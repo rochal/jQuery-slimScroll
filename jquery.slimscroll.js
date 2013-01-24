dfb69c3000c3070678a184e49f6a55a58d45476d
@rochal <piotr.rochala@gmail.com>
2013-01-24 23:55:57 +1100

    Fixed jQuery UI reference for compatibility with jQuery 1.9.0

--- jquery.slimscroll.js
+++ jquery.slimscroll.js
@@ -2,7 +2,7 @@
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * Version: 1.0.5
+ * Version: 1.0.6
  *
  */
 (function($) {
