1ac38d2cef968d17b32a6a6283ebf39f2e229f03
@rochal <piotr.rochala@gmail.com>
2013-03-11 14:10:27 +1100

    Add some meaningfull comments to the options section

--- jquery.slimscroll.js
+++ jquery.slimscroll.js
@@ -11,28 +11,66 @@
     slimScroll: function(options) {
 
       var defaults = {
-        wheelStep : 20,
+
+        // width in pixels of the visible scroll area
         width : 'auto',
+
+        // height in pixels of the visible scroll area
         height : '250px',
+
+        // width in pixels of the scrollbar and rail
         size : '7px',
+
+        // scrollbar color, accepts any hex/color value
         color: '#000',
+
+        // scrollbar position - left/right
         position : 'right',
+
+        // distance in pixels between the side edge and the scrollbar
         distance : '1px',
+
+        // default scroll position on load - top / bottom / $('selector')
         start : 'top',
+
+        // sets scrollbar opacity
         opacity : .4,
+
+        // enables always-on mode for the scrollbar
         alwaysVisible : false,
+
+        // check if we should hide the scrollbar when user is hovering over
         disableFadeOut: false,
+
+        // sets visibility of the rail
         railVisible : false,
+
+        // sets rail color
         railColor : '#333',
-        railOpacity : '0.2',
-        railClass : 'slimScrollRail',
-        barClass : 'slimScrollBar',
-        wrapperClass : 'slimScrollDiv',
-        allowPageScroll : false,
-        scroll : 0,
-        touchScrollStep : 200,
+
+        // sets rail opacity
+        railOpacity : .2,
+
         // whether  we should use jQuery UI Draggable to enable bar dragging
-        railDraggable : true
+        railDraggable : true,
+
+        // defautlt CSS class of the slimscroll rail
+        railClass : 'slimScrollRail',
+
+        // defautlt CSS class of the slimscroll bar
+        barClass : 'slimScrollBar',
+
+        // defautlt CSS class of the slimscroll wrapper
+        wrapperClass : 'slimScrollDiv',
+
+        // check if mousewheel should scroll the window if we reach top/bottom
+        allowPageScroll : false,
+
+        // scroll amount applied to each mouse wheel step
+        wheelStep : 20,
+
+        // scroll amount applied when user is using gestures
+        touchScrollStep : 200
       };
 
       var o = $.extend(defaults, options);
