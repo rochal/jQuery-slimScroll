5439a54df1db02a556f9476528eb1600e8acadef
edragame <edragame@gmail.com>
2013-09-30 13:21:42 -0400

    Update jquery.slimscroll.js to allow simple animated scrolls.

--- jquery.slimscroll.js
+++ jquery.slimscroll.js
@@ -74,6 +74,9 @@
 
         // sets border radius
         borderRadius: '7px',
+        
+         // sets animation status on a given scroll
+        animate: false,
 
         // sets border radius of the rail
         railBorderRadius : '7px'
@@ -363,7 +366,11 @@
           }
 
           // scroll content
-          me.scrollTop(delta);
+    	  if (o.animate){
+              me.animate({ scrollTop: delta });
+    	  }else{
+              me.scrollTop(delta);
+    	  }
 
           // fire scrolling event
           me.trigger('slimscrolling', ~~delta);
