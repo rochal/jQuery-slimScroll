e9a38f401068dcbd472e2efafaf0e69558f97c94
@rochal <piotr.rochala@gmail.com>
2013-12-27 18:03:13 +1100

    Up the version number.

--- jquery.slimscroll.js
+++ jquery.slimscroll.js
@@ -2,7 +2,7 @@
  * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
  * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
  *
- * Version: 1.3.1
+ * Version: 1.3.2
  *
  */
 (function($) {
